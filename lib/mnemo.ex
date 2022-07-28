b571b9908471a16912b21b9baf9502d7560502f2
Nikolai Kozak <nikokozak@gmail.com>
2022-07-28 11:42:02 -0400

    add tests

--- lib/mnemo.ex
+++ lib/mnemo.ex
@@ -3,81 +3,101 @@ defmodule Mnemo do
 
   @type t :: %{
     responsive: boolean, # whether to send out enqueued messages upon receipt of new messages.
-    to_send: queue,
+    send_queue: queue,
     messages: list(Message.t)
   }
   @type queue :: :queue.queue
 
-  defstruct [to_send: :queue.new(), messages: [], responsive: false]
+  defstruct [send_queue: :queue.new(), messages: [], responsive: false]
 
   def new, do: %__MODULE__{}
 
-  @spec schedule(t, Message.t) :: t
-  def schedule(%__MODULE__{} = mnemo, message) do
-    mnemo
-    |> Map.put(:to_send, enqueue_message(mnemo.to_send, message))
-  end
+  @spec has_enqueued?(t) :: boolean
+  def has_enqueued?(%Mnemo{} = mnemo), do: not :queue.is_empty(mnemo.send_queue)
 
-  @spec send_message(t) :: t
-  def send_message(mnemo) do
-    {to_send, mnemo} = dequeue_message(mnemo)
+  @spec has_received_or_sent?(t) :: boolean
+  def has_received_or_sent?(%Mnemo{} = mnemo), do: not Enum.empty?(mnemo.messages)
+
+  @spec enqueue_message(t, Message.t) :: t
+  def enqueue_message(%__MODULE__{} = mnemo, message), do: Map.put(mnemo, :send_queue, add_to_queue(mnemo.send_queue, message))
+
+  @spec send_enqueued_message(t) :: t
+  def send_enqueued_message(%Mnemo{} = mnemo) do
+    {to_send, mnemo} = remove_message_from_queue(mnemo)
 
     mnemo
     |> make_responsive
     |> archive_message(to_send)
   end
 
-  @spec receive_message(t, Message.t) :: t
-  def receive_message(mnemo, message) do
+  @spec send_message_now(t, Message.t) :: t
+  def send_message_now(%Mnemo{} = mnemo, nil), do: mnemo
+  def send_message_now(%Mnemo{} = mnemo, message) do
     mnemo
     |> archive_message(message)
-    |> inform_receipt()
+  end
+
+  @spec receive_message(t, Message.t) :: t
+  def receive_message(%Mnemo{ responsive: false } = mnemo, message) do
+    mnemo
+    |> archive_message(message)
+  end
+  def receive_message(%Mnemo{} = mnemo, message) do
+    mnemo
+    |> archive_message(message)
+    |> inform_receipt_to_queued_messages()
     |> send_messages_if_not_pending()
   end
 
-  def inform_receipt(mnemo) do
+  defp inform_receipt_to_queued_messages(%{ responsive: false } = mnemo), do: mnemo
+  defp inform_receipt_to_queued_messages(mnemo) do
     mnemo
-    |> Map.update!(:to_send, fn queue ->
+    |> Map.update!(:send_queue, fn queue ->
       update_first_in_queue(queue, &Message.ack_receipt/1)
     end)
   end
 
-  def send_messages_if_not_pending(%__MODULE__{responsive: true, to_send: {_, [%Message{listen_for: 0}]}} = mnemo), do: send_message(mnemo)
-  def send_messages_if_not_pending(%__MODULE__{responsive: true, to_send: {[%Message{listen_for: 0}], []}} = mnemo), do: send_message(mnemo)
-  def send_messages_if_not_pending(%__MODULE__{} = mnemo), do: mnemo
+  @spec send_messages_if_not_pending(t) :: t
+  defp send_messages_if_not_pending(%__MODULE__{responsive: true, send_queue: {_, [%Message{listen_for: 0}]}} = mnemo), do: send_enqueued_message(mnemo)
+  defp send_messages_if_not_pending(%__MODULE__{responsive: true, send_queue: {[%Message{listen_for: 0}], []}} = mnemo), do: send_enqueued_message(mnemo)
+  defp send_messages_if_not_pending(%__MODULE__{} = mnemo), do: mnemo
 
   @spec update_first_in_queue(queue, (Message.t -> Message.t)) :: queue
-  defp update_first_in_queue({_, []} = queue, _), do: queue
-  defp update_first_in_queue({[_first], []} = queue, func), do: do_update_first_in_queue(queue, func)
-  defp update_first_in_queue({_, [_first]} = queue, func), do: do_update_first_in_queue(queue, func)
-
-  defp do_update_first_in_queue(queue, func) do
-    {{:value, first}, queue} = :queue.out(queue)
-    :queue.in_r(func.(first), queue)
+  defp update_first_in_queue(queue, func) do
+    case :queue.out(queue) do
+      {:empty, _} -> queue
+      {{:value, first}, new_queue} -> :queue.in_r(func.(first), new_queue)
+    end
   end
 
   @spec make_responsive(t) :: t
-  defp make_responsive(mnemo), do: Map.put(mnemo, :responsive, true)
+  def make_responsive(mnemo), do: Map.put(mnemo, :responsive, true)
 
-  @spec enqueue_message(queue, any) :: queue
-  defp enqueue_message(queue, message), do: :queue.in(message, queue)
+  @spec make_unresponsive(t) :: t
+  def make_unresponsive(mnemo), do: Map.put(mnemo, :responsive, false)
 
-  @spec dequeue_message(t) :: { any | nil, t }
-  defp dequeue_message(mnemo) do
-    case :queue.out(mnemo.to_send) do
-      {{:value, val}, new_queue} -> { val, Map.put(mnemo, :to_send, new_queue) }
+
+  @spec remove_message_from_queue(t) :: { any | nil, t }
+  defp remove_message_from_queue(mnemo) do
+    case :queue.out(mnemo.send_queue) do
+      {{:value, val}, new_queue} -> { val, Map.put(mnemo, :send_queue, new_queue) }
       {:empty, _}           -> { nil, mnemo }
     end
   end
 
+  @spec archive_message(t, Message.t | nil) :: t
   defp archive_message(mnemo, nil), do: mnemo
   defp archive_message(mnemo, message) do
     Map.update!(mnemo, :messages, fn messages ->
-      add_message_to_list(messages, message)
+      add_to_list(messages, message)
     end)
   end
 
-  @spec add_message_to_list(list(), any) :: list()
-  defp add_message_to_list(message_list, message), do: message_list ++ [message]
+
+  @spec add_to_queue(queue, any) :: queue
+  defp add_to_queue(queue, value), do: :queue.in(value, queue)
+
+  @spec add_to_list(list(), any) :: list()
+  defp add_to_list(list, value), do: list ++ [value]
 
 end
--- test/mnemo_test.exs
+++ test/mnemo_test.exs
@@ -1,8 +1,110 @@
 defmodule MnemoTest do
   use ExUnit.Case
-  doctest Mnemo
+  alias Mnemo.Message
 
-  test "greets the world" do
-    assert Mnemo.hello() == :world
+  setup do
+    blank_mnemo = Mnemo.new()
+    {:ok, mnemo: blank_mnemo}
+  end
+
+  test "new/0 returns a new Mnemo struct" do
+    assert %{ send_queue: {[], []}, messages: [], responsive: false } = Mnemo.new()
+  end
+
+  test "enqueue_message/2 correctly enqueues a message", %{ mnemo: mnemo } do
+    message_1 = Message.new(%{ content: "one" })
+    message_2 = Message.new(%{ content: "two" })
+
+    assert %{ send_queue: {[^message_1], []} } = mnemo = Mnemo.enqueue_message(mnemo, message_1)
+    assert %{ send_queue: {[^message_2], [^message_1]} } = Mnemo.enqueue_message(mnemo, message_2)
+  end
+
+  describe "send_enqueued_message/1" do
+    test "sends enqueued message", %{mnemo: mnemo} do
+       message = Message.new(%{ content: "hey" })
+       mnemo = Mnemo.enqueue_message(mnemo, message)
+
+       assert %{ send_queue: {[], []}, messages: [^message] } = Mnemo.send_enqueued_message(mnemo)
+    end
+
+    test "does not change state if no messages are enqueued", %{mnemo: mnemo} do
+      assert %{ send_queue: {[], []}, messages: [] } = Mnemo.send_enqueued_message(mnemo)
+    end
+  end
+
+  test "send_message_now/2 adds a message to the messages archive without enqueueing it", %{mnemo: mnemo} do
+       message = Message.new(%{ content: "hey" })
+
+       assert %{ send_queue: {[], []}, messages: [^message] } = Mnemo.send_message_now(mnemo, message)
+  end
+
+  test "mnemo automatically becomes responsive after sending out queued message", %{mnemo: mnemo} do
+    assert %{ responsive: false } = mnemo
+
+    mnemo =
+      mnemo
+      |> Mnemo.enqueue_message(Message.new(%{ content: "one", listen_for: 2 }))
+      |> Mnemo.send_enqueued_message()
+
+    assert %{ responsive: true } = mnemo
+  end
+
+  describe "receive_message/2" do
+    test "places a message in the archive", %{mnemo: mnemo} do
+      message = Message.new(%{ content: "hey" })
+
+      assert %{ send_queue: {[], []}, messages: [^message] } = Mnemo.receive_message(mnemo, message)
+    end
+
+    test "reduces listen_for count on latest queued message if mnemo is responsive", %{mnemo: mnemo} do
+      mnemo =
+        mnemo
+        |> Mnemo.make_responsive()
+        |> Mnemo.enqueue_message(Message.new(%{ content: "one", listen_for: 2 }))
+        |> Mnemo.enqueue_message(Message.new(%{ content: "two", listen_for: 2 }))
+
+      mnemo = Mnemo.receive_message(mnemo, Message.new(%{ content: "received" }))
+
+      assert %{ send_queue: {[%{ content: "two", listen_for: 2 }], [%{ content: "one", listen_for: 1 }]} } = mnemo
+    end
+
+    test "if listen_for is zero on a queued messages, sends it out on receipt of message", %{mnemo: mnemo} do
+      mnemo =
+        mnemo
+        |> Mnemo.make_responsive()
+        |> Mnemo.enqueue_message(Message.new(%{ content: "one", listen_for: 2 }))
+        |> Mnemo.enqueue_message(Message.new(%{ content: "two", listen_for: 2 }))
+
+      mnemo = Mnemo.receive_message(mnemo, Message.new(%{ content: "received" }))
+
+      assert %{ send_queue: {[%{ content: "two", listen_for: 2 }], [%{ content: "one", listen_for: 1 }]} } = mnemo
+
+      mnemo = Mnemo.receive_message(mnemo, Message.new(%{ content: "received again" }))
+
+      assert %{ send_queue:
+                {[], [%{ content: "two", listen_for: 2 }]},
+                messages:
+                [%{ content: "received" }, %{content: "received again"}, %{content: "one", listen_for: 0}]
+              } = mnemo
+    end
+
+    test "receives a message without notifying queued messages if mnemo is unresponsive", %{mnemo: mnemo} do
+      mnemo =
+        mnemo
+        |> Mnemo.enqueue_message(Message.new(%{ content: "one", listen_for: 2 }))
+        |> Mnemo.enqueue_message(Message.new(%{ content: "two", listen_for: 2 }))
+
+      assert %{ responsive: false } = mnemo
+
+      mnemo = Mnemo.receive_message(mnemo, Message.new(%{ content: "received" }))
+
+      assert %{ send_queue: {[%{ content: "two", listen_for: 2 }], [%{ content: "one", listen_for: 2 }]} } = mnemo
+
+      mnemo = Mnemo.receive_message(mnemo, Message.new(%{ content: "another" }))
+
+      assert %{ send_queue: {[%{ content: "two", listen_for: 2 }], [%{ content: "one", listen_for: 2 }]}, messages: messages } = mnemo
+
+      assert length(messages) == 2
+    end
   end
 end
